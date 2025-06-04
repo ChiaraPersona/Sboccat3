5a66a02471338e4209e9cfc7a2ecb7412d66b2d1
Chiarabob <chiaraamodio97@gmail.com>
2025-06-04 20:30:55 +0200

    colore sfondo gioca

--- src/routes/gioca/+page.svelte
+++ src/routes/gioca/+page.svelte
@@ -1,12 +1,18 @@
 <script>
 	let frase = $state('sono gay');
+	let bgColorIndex = $state(0);
+
+	const colors = ['bg-sb-rosa', 'bg-sb-rosso', 'bg-sb-giallo', 'bg-sb-viola', 'bg-sb-arancione'];
 
 	function increase() {
 		frase = frase + 'y';
+		bgColorIndex = (bgColorIndex + 1) % colors.length;
 	}
 </script>
 
-<div class="flex h-[calc(100vh-124px-40px)] items-center justify-center p-8">
+<div
+	class={`flex h-[calc(100vh-124px-40px)] items-center justify-center p-8 transition-colors duration-500 ${colors[bgColorIndex]}`}
+>
 	<p class="w-full text-center text-4xl font-bold break-all uppercase">{frase}</p>
 </div>
 
@@ -16,7 +22,7 @@
 			e.preventDefault();
 			increase();
 		}}
-		class="bg-sb-rosso text-sb-bianco w-full touch-manipulation rounded-full p-4 text-xl font-bold uppercase hover:bg-red-700"
+		class="bg-sb-viola text-sb-bianco w-full touch-manipulation rounded-full p-4 text-xl font-bold uppercase hover:bg-purple-600"
 	>
 		di più
 	</button>
