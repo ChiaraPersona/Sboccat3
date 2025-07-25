482cc697ef084ae109f8a1eac89c2363bc4264c0
Chiarabob <chiaraamodio97@gmail.com>
2025-07-25 20:36:51 +0200

    eventi risolto

---
+++ src/routes/eventi/eventi.ts
@@ -0,0 +1,27 @@
+export type Evento = {
+	immagine: string;
+	titolo: string;
+	sottotitolo: string;
+	data: string;
+};
+
+export const eventi: Array<Evento> = [
+	{
+		immagine: 'desiderandae.png',
+		titolo: 'DESIDERANDAE',
+		sottotitolo: 'sottotitolo',
+		data: '23/09/2025'
+	},
+	{
+		immagine: 'doremixed.png',
+		titolo: 'HYPERPOP',
+		sottotitolo: 'sottotitolo',
+		data: '23/09/2025'
+	},
+	{
+		immagine: 'burn_this_way.png',
+		titolo: 'GAYMING',
+		sottotitolo: 'sottotitolo',
+		data: '23/09/2025'
+	}
+];
